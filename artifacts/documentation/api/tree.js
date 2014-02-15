784ac297eb15c321889c4efe7f517a7e10abb620
Woodhouse <contact@icecave.com.au>
2014-02-15 06:00:17 +0000

    Publishing artifacts from build 2.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -1,8 +1,5 @@
 var tree = [["Psr",,"",
                 [["Error","Psr\/Error.html","",
-                [["ErrorException","Psr\/Error\/ErrorException.html"," < ErrorException",
-                [        ]],["ErrorExceptionInterface","Psr\/Error\/ErrorExceptionInterface.html","",
-                [        ]],["ErrorHandler","Psr\/Error\/ErrorHandler.html","",
-                [        ]],["ErrorHandlerInterface","Psr\/Error\/ErrorHandlerInterface.html","",
+                [["ErrorHandler","Psr\/Error\/ErrorHandler.html","",
                 [        ]]        ]]        ]]        ]
 
