422c59125afdb8f5b75854b6aec5a382aa1a7b6b
Woodhouse <contact@icecave.com.au>
2014-03-04 07:32:55 +0000

    Publishing artifacts from build 14.

--- artifacts/documentation/api/search_index.js
+++ artifacts/documentation/api/search_index.js
@@ -1,7 +1,7 @@
 var search_data = {
     'index': {
         'searchIndex': ["psr","psr\\error","psr\\error\\errorhandler","psr\\error\\errorhandler::__invoke"],
-        'info': [["Psr","","Psr.html","","",3],["Psr\\Error","","Psr\/Error.html","","",3],["ErrorHandler","Psr\\Error","Psr\/Error\/ErrorHandler.html","","Handles PHP errors by converting them to error exceptions.",1],["ErrorHandler::__invoke","Psr\\Error\\ErrorHandler","Psr\/Error\/ErrorHandler.html#method___invoke","(integer $severity, string $message, string $path, integer $lineNumber)","Handle a PHP error.",2]]
+        'info': [["Psr","","Psr.html","","",3],["Psr\\Error","","Psr\/Error.html","","",3],["ErrorHandler","Psr\\Error","Psr\/Error\/ErrorHandler.html","","Handles PHP errors by converting them to error exceptions.",1],["ErrorHandler::__invoke","Psr\\Error\\ErrorHandler","Psr\/Error\/ErrorHandler.html#method___invoke","(integer $severity, string $message, string $path = &#039;&#039;, integer $lineNumber)","Handle a PHP error.",2]]
     }
 }
 search_data['index']['longSearchIndex'] = search_data['index']['searchIndex']
